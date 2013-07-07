c311755d2e5c3f74066a450d5dfc92ffdec7818e
Luis <Lfcunha@gmail.com>
2013-07-07 01:03:40 +0000

    fs read index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+
 var app = express.createServer(express.logger());
 
+var fs=require('fs');
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(fs.readFileSync('index.html', 'utf8');
 });
 
 var port = process.env.PORT || 5000;
