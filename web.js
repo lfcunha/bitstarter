19c5c1f451821bdbea8f66078a4a715457222351
Luis <Lfcunha@gmail.com>
2013-07-07 01:19:43 +0000

    use fs and buffer

--- web.js
+++ web.js
@@ -3,12 +3,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 var fs=require('fs');
 
 
+var bf=new Buffer(256);
+var buf=fs.readFileSync('index.html');
+var index=buf.toString('utf8');
+
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', 'utf8');
-});
+  response.send(index)});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
