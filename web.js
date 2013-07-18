5227d7bf2d952d3a369ce853dc11516378240192
lfcunha <lfcunha@gmail.com>
2013-07-18 01:41:39 +0000

    added image directory to web.js and images to html

--- web.js
+++ web.js
@@ -7,6 +7,11 @@ var app = express.createServer(express.logger());
 var fs=require('fs');
 
 
+app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/images'));
+
+
+
 var bf=new Buffer(256);
 var buf=fs.readFileSync('index.html');
 var index=buf.toString('utf8');
@@ -14,7 +19,7 @@ var index=buf.toString('utf8');
 app.get('/', function(request, response) {
   response.send(index)});
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
